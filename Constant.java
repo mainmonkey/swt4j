54ae30af1d6cb7771e90563d5785d77c3fc823c0
mainmonkey <mainmonkey@163.com>
2013-09-25 20:02:07 +0800

    Create Constant.java

---
+++ Constant.java
@@ -0,0 +1,9 @@
+package cn.shining.swt4j;
+
+public class Constant {
+	public static float neighbor_swt_ratio= 3f;
+	public static float variance_mean_ratio=0.8f;
+	public static float max_font_size=100f;
+	public static float min_font_size=10f;
+
+}
